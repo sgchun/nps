f59563740c9bc34fdec466bb5dd2808be5650eee
sgchun <sgchun@silver.broadinstitute.org>
2019-01-03 14:00:29 -0500

    Add a script to generate mfi files in non-ukbb data

---
+++ ukbb_support/make_snp_info.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+#####
+# Load qctool module here
+
+# (Broad institute)
+source /broad/software/scripts/useuse
+use .qctool-2.0-rc2
+#####
+
+BGEN_PATH_BASE=$1
+OUTPUT_DIR=$2
+
+for CHROM in `seq 1 22`; 
+do 
+    echo "chrom$CHROM:"
+
+    BGEN_PATH=`echo $BGEN_PATH_BASE | sed "s/\#/$CHROM/"`
+    QCTOOL_FILE="$OUTPUT_DIR/chrom$CHROM.qctool_snpstats.txt"
+    MFI_FILE="$OUTPUT_DIR/chrom$CHROM.mfi.txt"
+    
+    echo "    bgen input: $BGEN_PATH"
+
+    echo "    Running qctool..."
+    
+    qctool -g $BGEN_PATH -osnp $QCTOOL_FILE -snp-stats
+
+    echo "    Capturing the following columns:"
+    cat $QCTOOL_FILE |grep -v "^#"  | head -n 1 | cut -d' ' -f 1-2,4-6,14,15,17 | sed 's/ /\t/g'
+
+    echo "    SNP info output: $MFI_FILE"
+    cat $QCTOOL_FILE |grep -v "^#"  | tail -n +2 | cut -d' ' -f 1-2,4-6,14,15,17 | sed 's/ /\t/g' > $MFI_FILE
+
+
+    echo "    OK"
+
+done
+
+echo "Done"
